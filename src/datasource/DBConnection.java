457ea7d06915d32906975750a6078723c7b57503
Will_Zhu <will_zyq@qq.com>
2020-10-07 00:07:13 +0800

    solve local connection

--- src/datasource/DBConnection.java
+++ src/datasource/DBConnection.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
+
+import domain.Subject;
 
 public class DBConnection {
 	 private static final String url = "jdbc:postgresql://localhost:5432/Sample";
@@ -28,6 +31,7 @@ public class DBConnection {
 	 public static Connection connect() {
 		 Connection conn = null;
 		 try {
+			 Class.forName("org.postgresql.Driver");
 			 conn = DriverManager.getConnection(url, user, password);
 			 if (conn != null) {
 				 System.out.println("Connected to the PostgreSQL server successfully.");
@@ -36,12 +40,20 @@ public class DBConnection {
 			 }
 		 } catch (SQLException e) {
 			 System.out.println(e.getMessage());
-		 }
+		 } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			 System.out.println("forName Problem");
+			e.printStackTrace();
+		}
 		 return conn;
 	 }
 	 
 	 public static void main(String[] args) {
 		 DBConnection app = new DBConnection();
 		 app.connect();
+		 List<Subject> sub= Subject.getAllSubjects();
+   		 for (Subject subject : Subject.getAllSubjects()) {
+   			 System.out.println(subject.getSubjectCode());
+  		  } // for loop
 	 }
 }
